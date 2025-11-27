171a29858cd30eeea54422cba9af592f096c65c7
PedrUrchella13 <pedrourchella@gmail.com>
2025-11-27 15:04:37 -0300

    INICIE
    Adiciona classes Medico, Paciente e Tratamento
    
    Foram criadas três novas classes no namespace `BENEFICALIGHT.Models`:
    
    - `Medico`: Representa médicos com propriedades como `MedicoId`,
      `Nome`, `CRM`, entre outras.
    - `Paciente`: Representa pacientes com propriedades como `PacienteId`,
      `Nome`, `CPF`, `TipoSanguineo` e uma relação com `Medico`.
    - `Tratamento`: Representa tratamentos com propriedades como
      `TratamentoId`, `HorarioInicio`, `HorarioFim` e uma relação com
      `Paciente`.
    
    Essas mudanças estruturam o modelo de dados para o projeto.

---
+++ Models/Medico.cs
@@ -0,0 +1,15 @@
+﻿namespace BENEFICALIGHT.Models
+{
+    public class Medico
+    {
+        public int MedicoId { get; set; }
+        public string Nome { get; set; }
+        public int Idade { get; set; }
+        public DateOnly DataNascimento { get; set; }
+        public string Email { get; set; }
+        public int Telefone { get; set; }
+        public int CRM { get; set; }
+        public string Hospital { get; set; }
+
+    }
+}
